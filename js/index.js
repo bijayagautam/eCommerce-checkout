1166e5c9e9172bd7d1c56d01e0a961f6343e9d75
Bijaya Gautam <bijayagautam8@gmail.com>
2019-11-25 13:26:41 -0500

    Updated function with calculation logic.

--- js/index.js
+++ js/index.js
@@ -4,12 +4,23 @@ let addPantButton = document.getElementById("addPantButton");
 let subShirtButton = document.getElementById("substractShirtButton");
 let addShirtButton = document.getElementById("addShirtButton");
 
+let calButton = document.getElementById("calculateButton");
+
 let pantQuantity = 0;
 let pantUnitPrice = 49.99;
 let extendedPrice = 0;
+
+let shirtQuantity = 0;
+let shirtUnitPrice = 10.99;
+
 document.getElementById(`pantQuantityLabel`).innerHTML = `${pantQuantity}`;
-document.getElementById(`pantUnitPriceLabel`).innerHTML = `$ ${pantUnitPrice}`;
-document.getElementById(`pantExtendedPriceLabel`).innerHTML = `$ ${extendedPrice}`;
+document.getElementById(`pantUnitPriceLabel`).innerHTML = `${pantUnitPrice}`;
+document.getElementById(`pantExtendedPriceLabel`).innerHTML = `${extendedPrice}`;
+
+document.getElementById(`shirtQuantityLabel`).innerHTML = `${shirtQuantity}`;
+document.getElementById(`shirtUnitPriceLabel`).innerHTML = `${shirtUnitPrice}`;
+document.getElementById(`shirtExtendedPriceLabel`).innerHTML = `${extendedPrice}`;
+
 
 
 function subQty(quantity){
@@ -31,27 +42,54 @@ function getPrice(price, quantity){
 
 function subPantQty(){
     let pantqty = subQty(document.getElementById(`pantQuantityLabel`).innerHTML);
+    let pantUnitPrice = 49.99;
+    let extendedPrice = getPrice(pantUnitPrice,pantqty)
     document.getElementById(`pantQuantityLabel`).innerHTML = `${pantqty}`;
+    document.getElementById(`pantUnitPriceLabel`).innerHTML = `${pantUnitPrice}`;
+    document.getElementById(`pantExtendedPriceLabel`).innerHTML = `${extendedPrice}`;
 }
 
 function addPantQty(){
     let pantqty = addQty(document.getElementById(`pantQuantityLabel`).innerHTML);
     let pantUnitPrice = 49.99;
     let extendedPrice = getPrice(pantUnitPrice,pantqty)
+
     document.getElementById(`pantQuantityLabel`).innerHTML = `${pantqty}`;
-    document.getElementById(`pantUnitPriceLabel`).innerHTML = `$ ${pantUnitPrice}`;
-    document.getElementById(`pantExtendedPriceLabel`).innerHTML = `$ ${extendedPrice}`;
+    document.getElementById(`pantUnitPriceLabel`).innerHTML = `${pantUnitPrice}`;
+    document.getElementById(`pantExtendedPriceLabel`).innerHTML = `${extendedPrice}`;
 
 }
 
 function subShirtQty(){
     let shirtqty = subQty(document.getElementById(`shirtQuantityLabel`).innerHTML);
+    let shirtUnitPrice = 10.99;
+    let extendedPrice = getPrice(shirtUnitPrice,shirtqty)
+
     document.getElementById(`shirtQuantityLabel`).innerHTML = `${shirtqty}`;
+    document.getElementById(`shirtUnitPriceLabel`).innerHTML = `${shirtUnitPrice}`;
+    document.getElementById(`shirtExtendedPriceLabel`).innerHTML = `${extendedPrice}`;
 }
 
 function addShirtQty(){
     let shirtqty = addQty(document.getElementById(`shirtQuantityLabel`).innerHTML);
+    let shirtUnitPrice = 10.99;
+    let extendedPrice = getPrice(shirtUnitPrice,shirtqty)
+
     document.getElementById(`shirtQuantityLabel`).innerHTML = `${shirtqty}`;
+    document.getElementById(`shirtUnitPriceLabel`).innerHTML = `${shirtUnitPrice}`;
+    document.getElementById(`shirtExtendedPriceLabel`).innerHTML = `${extendedPrice}`;
+}
+
+function calculateDetail(){
+    let pantExtPrice = parseFloat(document.getElementById("pantExtendedPriceLabel").innerHTML);
+    let shirtExtPrice = parseFloat(document.getElementById("shirtExtendedPriceLabel").innerHTML);
+    let result = pantExtPrice + shirtExtPrice;
+    let tax = result * 13/100;
+    let total = result + tax;
+
+    document.getElementById(`subtotalLabel`).innerHTML = `${result.toFixed(2)}`;
+    document.getElementById(`taxLabel`).innerHTML = `${tax.toFixed(2)}`;
+    document.getElementById(`totalLabel`).innerHTML = `${total.toFixed(2)}`;
 }
 
 subPantButton.addEventListener("click",subPantQty)
@@ -59,3 +97,5 @@ addPantButton.addEventListener("click",addPantQty)
 
 subShirtButton.addEventListener("click",subShirtQty)
 addShirtButton.addEventListener("click",addShirtQty)
+
+calButton.addEventListener("click",calculateDetail)
