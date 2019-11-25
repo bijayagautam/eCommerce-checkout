c422dacf5bed39655b4de65b912c29d35d2d9459
Bijaya Gautam <bijayagautam8@gmail.com>
2019-11-25 00:42:06 -0500

    Aded Updated function.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@ let addPantButton = document.getElementById("addPantButton");
 let subShirtButton = document.getElementById("substractShirtButton");
 let addShirtButton = document.getElementById("addShirtButton");
 
+let pantQuantity = 0;
+let pantUnitPrice = 49.99;
+let extendedPrice = 0;
+document.getElementById(`pantQuantityLabel`).innerHTML = `${pantQuantity}`;
+document.getElementById(`pantUnitPriceLabel`).innerHTML = `$ ${pantUnitPrice}`;
+document.getElementById(`pantExtendedPriceLabel`).innerHTML = `$ ${extendedPrice}`;
+
 
 function subQty(quantity){
     if (quantity <= 0){
@@ -18,6 +25,10 @@ function addQty(quantity){
     return(++quantity);
 }
 
+function getPrice(price, quantity){
+    return(price * quantity);
+}
+
 function subPantQty(){
     let pantqty = subQty(document.getElementById(`pantQuantityLabel`).innerHTML);
     document.getElementById(`pantQuantityLabel`).innerHTML = `${pantqty}`;
@@ -25,7 +36,12 @@ function subPantQty(){
 
 function addPantQty(){
     let pantqty = addQty(document.getElementById(`pantQuantityLabel`).innerHTML);
+    let pantUnitPrice = 49.99;
+    let extendedPrice = getPrice(pantUnitPrice,pantqty)
     document.getElementById(`pantQuantityLabel`).innerHTML = `${pantqty}`;
+    document.getElementById(`pantUnitPriceLabel`).innerHTML = `$ ${pantUnitPrice}`;
+    document.getElementById(`pantExtendedPriceLabel`).innerHTML = `$ ${extendedPrice}`;
+
 }
 
 function subShirtQty(){
@@ -37,8 +53,6 @@ function addShirtQty(){
     let shirtqty = addQty(document.getElementById(`shirtQuantityLabel`).innerHTML);
     document.getElementById(`shirtQuantityLabel`).innerHTML = `${shirtqty}`;
 }
-
-
 
 subPantButton.addEventListener("click",subPantQty)
 addPantButton.addEventListener("click",addPantQty)
